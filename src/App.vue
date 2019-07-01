fa5b1583fbf9e3149678a473894ca51dd7c520a4
hanshuai291 <a1304437502@gmail.com>
2019-07-01 08:38:52 +0800

    init my project

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="content">
+    <mt-header fixed title="饿了么"></mt-header>
+    <h1>这是 App 组件</h1>
+
+    		<nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">外卖</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">发现</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">订单</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">我的</span>
+			</a>
+		</nav>
+  </div>
+</template>
+
+<script>
+</script>
+
+
+<style lang="scss" scoped>
+.content{
+  padding-top: 40px
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+// 入口文件
+import Vue from "vue"
+import app from "./App.vue"
+
+//按需导入header组件
+import {Header} from "mint-ui"
+import "./lib/mui/css/mui.css"
+
+Vue.component(Header.name,Header)
+
+
+let vm = new Vue({
+    el:"#app",
+    render:c=>c(app)
+})
